68200dcdccfa671de0a31b69ad19aea6b203f36f
chaeplin <chaeplin@gmail.com>
2016-08-13 01:04:55 +0900

    adding reporting

--- nest-bridge/python/nest.py
+++ nest-bridge/python/nest.py
@@ -6,9 +6,11 @@ import json
 import socket
 import sys
 import yaml
+import time
 
 import paho.mqtt.client as mqtt
 import ssl
+
 
 def on_connect(client, userdata, flags, rc):
     print ("Connected with result code "+str(rc))
@@ -17,17 +19,48 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(mqtt_subscribe + "setTargetHeatingCoolingState", 0)
 
 def on_message(client, userdata, msg):
+    global first_setAway
+    global first_setTargetHeatingCoolingState
+    global first_setTargetTemperature
+    global ts
+
     print (msg.topic+" "+str(msg.payload))
+    now = time.time()
     if (msg.topic == mqtt_subscribe + "setAway"):
-        homeaway_set.put(dict_Away[str(msg.payload)])
+        if (first_setAway == False):
+            if ((now - ts) > 60):
+                homeaway_set.put(dict_Away[str(msg.payload)])
+                ts = time.time()
+        first_setAway = False
+    elif (msg.topic == mqtt_subscribe + "setTargetHeatingCoolingState"):
+        if (first_setTargetHeatingCoolingState == False):
+            if ((now - ts) > 60):
+                thermostat_set.put(dict_Hvac_mode[str(msg.payload)])
+                ts = time.time()
+        first_setTargetHeatingCoolingState = False
+    elif (msg.topic == mqtt_subscribe + "setTargetTemperature"):
+        if (first_setTargetTemperature == False):
+            if ((now - ts) > 60):
+                thermostat_set.put({"target_temperature_c": float(msg.payload)})
+                ts = time.time()
+        first_setTargetTemperature = False
+    else:
+        print("should not")
 
-    if (msg.topic == mqtt_subscribe + "setTargetHeatingCoolingState"):
-        thermostat_set.put(dict_Hvac_mode[str(msg.payload)])
-
-    if (msg.topic == mqtt_subscribe + "setTargetTemperature"):
-        thermostat_set.put({"target_temperature_c": float(msg.payload)})
+    if ((now - ts) <= 60):
+        if (first == False):
+            print (now - ts)
+            payload = "{\"Away\":" + away_state + ",\"TargetHeatingCoolingState\":" + hvac_target + ",\"CurrentTemperature\":" + str(ambient_temperature_c) + ",\"CurrentRelativeHumidity\":" + str(humidity) + ",\"TargetTemperature\":" + str(target_temperature_c) + "}"
+            client.publish(mqtt_publish, payload, 0, 1)
 
 def p(x):
+    global away_state
+    global hvac_target
+    global ambient_temperature_c
+    global humidity 
+    global target_temperature_c
+    global first
+
     result = json.loads(x[1])
 
     if len(result['data']) == 3:
@@ -39,6 +72,39 @@ def p(x):
         print ("---> is_online : %s") % result['data']['devices']['thermostats'][device_id]['is_online']
         print ("---> hvac_state : %s") % result['data']['devices']['thermostats'][device_id]['hvac_state']
         print ("---> away_state : %s") % result['data']['structures'][structure_id]['away']    
+
+        away                  = result['data']['structures'][structure_id]['away']
+        target_temperature_c  = result['data']['devices']['thermostats'][device_id]['target_temperature_c'] 
+        humidity              = result['data']['devices']['thermostats'][device_id]['humidity']
+        ambient_temperature_c = result['data']['devices']['thermostats'][device_id]['ambient_temperature_c']
+        hvac_mode             = result['data']['devices']['thermostats'][device_id]['hvac_mode']       
+
+
+        if away == 'home':
+            away_state = '0'
+        elif away == 'away':
+            away_state = '1'
+        elif away == 'auto-away':
+            away_state = '1'
+        else:
+            print("should not")
+
+        if hvac_mode == 'off':
+            hvac_target = '0'
+        elif hvac_mode == 'heat':
+            hvac_target = '1'
+        elif hvac_mode == 'cool':
+            hvac_target = '2'
+        elif hvac_mode == 'heat-cool':
+            hvac_target = '3'
+        else:
+            print("should not")
+
+        payload = "{\"Away\":" + away_state + ",\"TargetHeatingCoolingState\":" + hvac_target + ",\"CurrentTemperature\":" + str(ambient_temperature_c) + ",\"CurrentRelativeHumidity\":" + str(humidity) + ",\"TargetTemperature\":" + str(target_temperature_c) + "}"
+        client.publish(mqtt_publish, payload, 0, 1)
+        
+        if ( first == True):
+            first = False
 
 #---------
 config = yaml.load(open('conf/nest.conf', 'r'))
@@ -58,22 +124,13 @@ mqtt_user      = config.get('mqtt_user')
 mqtt_pass      = config.get('mqtt_pass')
 mqtt_clientid  = config.get('mqtt_clientid')
 mqtt_subscribe = config.get('mqtt_subscribe')
-
-#setAway
-#setTargetTemperature
-#setTargetHeatingCoolingState
-#
-#isAway
-#getTargetTemperature
-#getTargetHeatingCoolingState
-#getCurrentTemperature
-#getCurrentRelativeHumidity
-#getCurrentHeatingCoolingState
-#
+mqtt_publish   = config.get('mqtt_publish')
 
 dict_Away = {
     '0': {"away": "home"},
-    '1': {"away": "away"}
+    '1': {"away": "away"},
+    'false': {"away": "home"},
+    'True': {"away": "away"}
 }
 
 dict_Hvac_mode = {
@@ -82,6 +139,13 @@ dict_Hvac_mode = {
     '2': {"hvac_mode": "cool"},
     '3': {"hvac_mode": "heat-cool"}
 }
+
+
+first = True
+first_setAway  = True
+first_setTargetHeatingCoolingState = True
+first_setTargetTemperature = True
+ts = time.time()
 
 client = mqtt.Client(client_id=mqtt_clientid)
 client.on_connect = on_connect
@@ -96,8 +160,8 @@ fb = Firebase('https://developer-api.nest.com/')
 
 # Or use a custom callback
 custom_callback = fb.child(streaming_path).listener(p)
-homeaway_set    = fb.child(homeaway_path)
-thermostat_set  = fb.child(thermostat_path)
+homeaway_set   = fb.child(homeaway_path)
+thermostat_set = fb.child(thermostat_path)
 
 try:
     custom_callback.start()
